4743954ef3ebc6376d5e80faa5702e540cc7f212
Nguyễn Thế Sơn <55500547+Son-Nguyen48@users.noreply.github.com>
2023-09-05 21:22:18 +0700

    change link api

--- src/components/app/The-Project/TheInbox.vue
+++ src/components/app/The-Project/TheInbox.vue
@@ -187,7 +187,7 @@ const route = useRoute()
 
 let taskList = ref([])
 axios
-  .get('http://localhost:3000/Backend/api/getAllTodo/')
+  .get('https://sonnguyen48todoist.000webhostapp.com/api/getAllTask/')
   .then((response) => {
     taskList.value = response.data.taskList
   })
