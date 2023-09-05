6594ff714e9b317d153b9f8c020171a50d2921b1
Nguyễn Thế Sơn <55500547+Son-Nguyen48@users.noreply.github.com>
2023-09-06 00:43:54 +0700

    code coating for modal

--- src/components/UI/Modal/TheTask.vue
+++ src/components/UI/Modal/TheTask.vue
@@ -1,15 +1,18 @@
 <template>
   <div
-    class="fixed p-5 shadow-xl w-[750px] h-[450px] rounded-md top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 z-[100] bg-white"
+    class="fixed p-5 shadow-xl w-[750px] h-[450px] rounded-md top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 z-[999] bg-white"
   >
-    <span>The Task</span>
-    <span>{{ route.params }}</span>
-    <RouterLink
-      class="px-4 py-2 border-[1px] bg-red-200 hover:transform hover:scale-105 rounded-md"
-      :to="{ name: 'app-inbox', params: { idProject: 'p1' } }"
-      >Back</RouterLink
-    >
+    <div class="flex items-center gap-2">
+      <span>The Task</span>
+      <span>{{ route.params }}</span>
+      <RouterLink
+        class="px-4 py-2 border-[1px] bg-red-200 hover:transform hover:scale-105 rounded-md"
+        :to="{ name: 'app-inbox', params: { idProject: 'p1' } }"
+        >Back</RouterLink
+      >
+    </div>
   </div>
+  <div class="h-screen w-screen bg-slate-300 opacity-60 fixed top-0 left-0 z-[100]"></div>
 </template>
 
 <script setup>
--- src/components/app/The-Project/TheInbox.vue
+++ src/components/app/The-Project/TheInbox.vue
@@ -7,119 +7,122 @@
           <li
             v-for="task in taskList"
             :key="task.id"
-            class="goal relative cursor-pointer flex gap-2 before:content-[''] before:absolute before:-left-7 before:w-[100px] before:h-full py-2"
+            class="goal relative cursor-pointer before:content-[''] before:absolute before:-left-7 before:w-[100px] before:h-full"
           >
-            <button
-              class="hover:bg-[#f5f5f5] hover:cursor-move rounded-md hidden absolute -left-7 drag_button"
-            >
-              <svg width="24" height="24">
-                <path
-                  fill="currentColor"
-                  d="M14.5 15.5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 15.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 15.5zm5-5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 10.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 10.5zm5-5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 5.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 5.5z"
-                ></path>
-              </svg>
-            </button>
+            <div class="flex gap-2 py-3">
+              <button
+                class="hover:bg-[#f5f5f5] hover:cursor-move rounded-md hidden absolute -left-7 drag_button"
+              >
+                <svg width="24" height="24">
+                  <path
+                    fill="currentColor"
+                    d="M14.5 15.5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 15.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 15.5zm5-5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 10.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 10.5zm5-5a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 0114.5 5.5zm-5 0a1.5 1.5 0 11-.001 3.001A1.5 1.5 0 019.5 5.5z"
+                  ></path>
+                </svg>
+              </button>
 
-            <button class="relative top-1 border-[1px] rounded-full h-5 w-5">
-              <svg width="24" height="24" class="text-[#E5E7EB] absolute -top-[3px] -left-[4px]">
-                <path
-                  fill="currentColor"
-                  d="M11.23 13.7l-2.15-2a.55.55 0 0 0-.74-.01l.03-.03a.46.46 0 0 0 0 .68L11.24 15l5.4-5.01a.45.45 0 0 0 0-.68l.02.03a.55.55 0 0 0-.73 0l-4.7 4.35z"
-                ></path>
-              </svg>
-            </button>
+              <button class="relative top-1 border-[1px] rounded-full h-5 w-5">
+                <svg width="24" height="24" class="text-[#E5E7EB] absolute -top-[3px] -left-[4px]">
+                  <path
+                    fill="currentColor"
+                    d="M11.23 13.7l-2.15-2a.55.55 0 0 0-.74-.01l.03-.03a.46.46 0 0 0 0 .68L11.24 15l5.4-5.01a.45.45 0 0 0 0-.68l.02.03a.55.55 0 0 0-.73 0l-4.7 4.35z"
+                  ></path>
+                </svg>
+              </button>
 
-            <RouterLink
-              :to="{ name: 'project-task-detail', params: { idTask: '1234' } }"
-              class="flex w-full"
-            >
-              <div>
-                <p>{{ task.title }}</p>
-                <div class="flex items-center gap-2">
-                  <span>
+              <RouterLink
+                :to="{ name: 'project-task-detail', params: { idTask: '1234' } }"
+                class="flex w-full"
+              >
+                <div>
+                  <p>{{ task.title }}</p>
+                  <div class="flex items-center gap-2 mt-2">
+                    <span>
+                      <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="calendar_icon text-red-400"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M9.5 1h-7A1.5 1.5 0 001 2.5v7A1.5 1.5 0 002.5 11h7A1.5 1.5 0 0011 9.5v-7A1.5 1.5 0 009.5 1zM2 2.5a.5.5 0 01.5-.5h7a.5.5 0 01.5.5v7a.5.5 0 01-.5.5h-7a.5.5 0 01-.5-.5v-7zM8.75 8a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM3.5 4a.5.5 0 000 1h5a.5.5 0 000-1h-5z"
+                          fill="currentColor"
+                        ></path>
+                      </svg>
+                    </span>
+                    <span class="text-[12px] text-red-400">{{ task.dueDate }}</span>
+                  </div>
+                </div>
+                <div class="goal_control ml-auto hidden items-center gap-2">
+                  <button class="hover:bg-[#f5f5f5] rounded-sm">
+                    <svg width="24" height="24">
+                      <g fill="none" fill-rule="evenodd">
+                        <path
+                          fill="currentColor"
+                          d="M9.5 19h10a.5.5 0 110 1h-10a.5.5 0 110-1z"
+                        ></path>
+                        <path
+                          stroke="currentColor"
+                          d="M4.42 16.03a1.5 1.5 0 00-.43.9l-.22 2.02a.5.5 0 00.55.55l2.02-.21a1.5 1.5 0 00.9-.44L18.7 7.4a1.5 1.5 0 000-2.12l-.7-.7a1.5 1.5 0 00-2.13 0L4.42 16.02z"
+                        ></path>
+                      </g>
+                    </svg>
+                  </button>
+
+                  <button class="hover:bg-[#f5f5f5] rounded-sm">
                     <svg
-                      width="12"
-                      height="12"
-                      viewBox="0 0 12 12"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
-                      class="calendar_icon text-red-400"
                     >
                       <path
                         fill-rule="evenodd"
                         clip-rule="evenodd"
-                        d="M9.5 1h-7A1.5 1.5 0 001 2.5v7A1.5 1.5 0 002.5 11h7A1.5 1.5 0 0011 9.5v-7A1.5 1.5 0 009.5 1zM2 2.5a.5.5 0 01.5-.5h7a.5.5 0 01.5.5v7a.5.5 0 01-.5.5h-7a.5.5 0 01-.5-.5v-7zM8.75 8a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM3.5 4a.5.5 0 000 1h5a.5.5 0 000-1h-5z"
+                        d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2zM5 6a1 1 0 011-1h12a1 1 0 011 1v12a1 1 0 01-1 1H6a1 1 0 01-1-1V6zm12 10a1 1 0 11-2 0 1 1 0 012 0zM7 8a.5.5 0 000 1h10a.5.5 0 000-1H7z"
                         fill="currentColor"
                       ></path>
                     </svg>
-                  </span>
-                  <span class="text-[12px] text-red-400">{{ task.dueDate }}</span>
-                </div>
-              </div>
-              <div class="goal_control ml-auto hidden items-center gap-2">
-                <button class="hover:bg-[#f5f5f5] rounded-sm">
-                  <svg width="24" height="24">
-                    <g fill="none" fill-rule="evenodd">
+                  </button>
+
+                  <RouterLink
+                    :to="{ name: 'project-task-detail', params: { idTask: '1234' } }"
+                    class="hover:bg-[#f5f5f5] rounded-sm"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      data-svgs-path="sm1/comments.svg"
+                    >
                       <path
                         fill="currentColor"
-                        d="M9.5 19h10a.5.5 0 110 1h-10a.5.5 0 110-1z"
+                        fill-rule="nonzero"
+                        d="M11.707 20.793A1 1 0 0 1 10 20.086V18H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4.5l-2.793 2.793zM11 20.086L14.086 17H19a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h6v3.086z"
                       ></path>
+                    </svg>
+                  </RouterLink>
+
+                  <button
+                    class="hover:bg-[#f5f5f5] rounded-sm h-6 w-6 flex justify-center items-center"
+                  >
+                    <svg width="15" height="3">
                       <path
-                        stroke="currentColor"
-                        d="M4.42 16.03a1.5 1.5 0 00-.43.9l-.22 2.02a.5.5 0 00.55.55l2.02-.21a1.5 1.5 0 00.9-.44L18.7 7.4a1.5 1.5 0 000-2.12l-.7-.7a1.5 1.5 0 00-2.13 0L4.42 16.02z"
+                        d="M1.5 3a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm6 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm6 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
+                        fill="currentColor"
+                        fill-rule="evenodd"
                       ></path>
-                    </g>
-                  </svg>
-                </button>
-
-                <button class="hover:bg-[#f5f5f5] rounded-sm">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2zM5 6a1 1 0 011-1h12a1 1 0 011 1v12a1 1 0 01-1 1H6a1 1 0 01-1-1V6zm12 10a1 1 0 11-2 0 1 1 0 012 0zM7 8a.5.5 0 000 1h10a.5.5 0 000-1H7z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                </button>
-
-                <RouterLink
-                  :to="{ name: 'project-task-detail', params: { idTask: '1234' } }"
-                  class="hover:bg-[#f5f5f5] rounded-sm"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    data-svgs-path="sm1/comments.svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      fill-rule="nonzero"
-                      d="M11.707 20.793A1 1 0 0 1 10 20.086V18H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4.5l-2.793 2.793zM11 20.086L14.086 17H19a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h6v3.086z"
-                    ></path>
-                  </svg>
-                </RouterLink>
-
-                <button
-                  class="hover:bg-[#f5f5f5] rounded-sm h-6 w-6 flex justify-center items-center"
-                >
-                  <svg width="15" height="3">
-                    <path
-                      d="M1.5 3a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm6 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm6 0a1.5 1.5 0 110-3 1.5 1.5 0 010 3z"
-                      fill="currentColor"
-                      fill-rule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </RouterLink>
+                    </svg>
+                  </button>
+                </div>
+              </RouterLink>
+            </div>
+            <hr />
           </li>
           <hr />
         </ul>
@@ -187,7 +190,7 @@ const route = useRoute()
 
 let taskList = ref([])
 axios
-  .get('https://sonnguyen48todoist.000webhostapp.com/api/getAllTask/')
+  .get('http://localhost:3000/api/getAllTodo/')
   .then((response) => {
     taskList.value = response.data.taskList
   })
